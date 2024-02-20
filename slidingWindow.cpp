53a799e1abcff8a4ed5912b236881da0dc5fb5b0
Youssef Abd-Elkarim <90554178+YAbdelkarim@users.noreply.github.com>
2024-02-20 14:20:24 +0200

    Update slidingWindow.cpp

--- slidingWindow.cpp
+++ slidingWindow.cpp
@@ -3,31 +3,26 @@ using namespace std;
 
 int biggestSum(int arr[], int size, int k)
 {
-    int max_sum = INT_MIN;
-    for (size_t i = 0; i < size - k + 1; i++)
+    int max_sum = 0;
+
+    for (size_t i = 0; i < k; i++)
     {
-        int current_sum = 0;
-        for (size_t j = 0; j < k; j++)
-        {
-            current_sum += arr[i + j];
-        }
-        max_sum = max(max_sum, current_sum);
+        max_sum += arr[i];
     }
+
+    int window_sum = max_sum;
+    for (size_t i = k; i < size; i++)
+    {
+        window_sum += arr[i] - arr[i - k];
+        max_sum = max(window_sum, max_sum);
+    }
+
     return max_sum;
 }
 
 int main()
 {
-
-    int arr[] = {5,
-                 4,
-                 2,
-                 3,
-                 -1,
-                 9,
-                 3,
-                 7,
-                 6};
+    int arr[] = {5, 4, 2, 3, -1, 9, 3, 7, 6};
     int k = 4;
     int size = sizeof(arr) / sizeof(arr[0]);
 
